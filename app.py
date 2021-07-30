03b8019900ec03e05908b1106c586f70e78cd2b4
Jason He <64624381+vergica@users.noreply.github.com>
2021-07-30 16:03:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
 from flask import Flask, render_template
-import pck.getinfo
+import random
+import re
+from lxml import etree
 app = Flask(__name__)
+
+
+def get_text():
+    d = {}
+    for i in open("word.txt", "r", encoding='utf-8').readlines():
+        d[re.findall(r'\..*?<(.*?)\n', i)[0]] = re.findall(r'\.(.*?)<.*?\n', i)[0]
+    m = random.sample(d.keys(), 4)
+    m.insert(0, d[m[0]].strip(' '))
+    return m
+
+
+def get_zh(w):
+    html = etree.parse("http://cn.bing.com/dict/search?q=" + w, etree.HTMLParser())
+    d = []
+    en = html.xpath('//*[@id="sentenceSeg"]/div/div[2]/div[1]')
+    zh = html.xpath('//*[@id="sentenceSeg"]/div/div[2]/div[2]')
+    for i in range(len(en)):
+        if re.match(r'.*?\. \. \.', en[i].xpath("string(.)")) is None:
+            try:
+                d.append([en[i].xpath("string(.)"), zh[i].xpath("string(.)").encode('ISO-8859-1').decode('utf-8')])
+            except Exception:
+                pass
+    return random.sample(d, 3)
 
 
 @app.route('/')
 def index():
-    t = pck.getinfo.get_text()
+    t =get_text()
     return render_template(
         'index.html',
         text=t,
-        zhs=pck.getinfo.get_zh(t[0])
+        zhs=get_zh(t[0])
     )
 
 
